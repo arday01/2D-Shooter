4407c0452570ed3773d84bc943a21beef1b9d68c
Burak Arday <bburakarday@gmail.com>
2024-03-05 15:54:20 +0300

    Player Shoot

--- Assets/Scripts/Game/Player/Bullet.cs
+++ Assets/Scripts/Game/Player/Bullet.cs
@@ -1,18 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision) //herhangi bir yere çarpınca fark edilcek
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.GetComponent<EnemyMovement>())
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);//enemy yok eder
+        }
+       
     }
 }
--- Assets/Scripts/Game/Player/PlayerShoot.cs
+++ Assets/Scripts/Game/Player/PlayerShoot.cs
@@ -1,21 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerShoot : MonoBehaviour
 {
     [SerializeField]private GameObject bulletPrefab;
 
-    private float bulletSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]private float bulletSpeed;
+
+    [SerializeField] private Transform gunOffset;
+    [SerializeField]private float timeBetweenShots;
+    
+    private bool fireContinuously;
+    private bool fireSingle;
+    private float lastFireTime;
+    
 
     // Update is called once per frame
     void Update()
     {
+        if (fireContinuously || fireSingle)
+        {
+            float timeSinceLastFire = Time.time - lastFireTime;
+            
+            if (timeSinceLastFire>=timeBetweenShots)
+            {
+                FireBullet();
+                lastFireTime = Time.time;
+                fireSingle = false;
+            }
+           
+        }
+
         
+    }
+
+    private void  FireBullet ()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, gunOffset.position, transform.rotation);
+        Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D>();
+        rigidbody.velocity = bulletSpeed * transform.up;
+    }
+
+    private void OnFire(InputValue inputValue)
+    {
+        fireContinuously = inputValue.isPressed;
+        
+        if (inputValue.isPressed)
+        {
+            fireSingle = true;
+        }
     }
 }
