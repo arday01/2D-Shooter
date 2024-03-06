2d6183b77a23231e071e5e5043355ae76a56e67c
Burak Arday <bburakarday@gmail.com>
2024-03-06 10:36:14 +0300

    Destroying Bullets
    Destroying Bullets When They Leave the Screen

--- Assets/Scripts/Game/Player/Bullet.cs
+++ Assets/Scripts/Game/Player/Bullet.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        DestroyWhenOffScreen();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) //herhangi bir yere çarpınca fark edilcek
     {
         if (collision.GetComponent<EnemyMovement>())
@@ -13,5 +25,15 @@ public class Bullet : MonoBehaviour
             Destroy(gameObject);//enemy yok eder
         }
        
+    }
+
+    private void DestroyWhenOffScreen()
+    {
+        Vector2 screenPosition = camera.WorldToScreenPoint(transform.position);
+
+        if (screenPosition.x<0 || screenPosition.x>camera.pixelWidth || screenPosition.y<0 || screenPosition.y>camera.pixelHeight)
+        {
+            Destroy(gameObject);
+        }
     }
 }
